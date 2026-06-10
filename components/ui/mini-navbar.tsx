a395561d0886e80caeb8606cf945c12af70d978f
a3ro-dev <akshatsingh14372@outlook.com>
2026-06-10 13:10:39 +0530

    fix(ui): navbar logo invert logic for high contrast in light mode

--- components/ui/mini-navbar.tsx
+++ components/ui/mini-navbar.tsx
@@ -119,7 +119,7 @@ export function MiniNavbar() {
                             alt="bits&bytes™ logo"
                             width={26}
                             height={26}
-                            className="object-contain dark:invert"
+                            className="object-contain invert dark:invert-0"
                         />
                     </div>
                 </Link>
